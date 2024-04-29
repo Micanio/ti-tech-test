0d02408a1cadda04af948af9c1c6d3d353ebe989
Michael McCann <micanio@googlemail.com>
2024-04-29 10:37:12 +0100

    Started pulling data from bundles.json and displaying on screen

--- src/components/Bundle.vue
+++ src/components/Bundle.vue
@@ -1,10 +1,16 @@
 <template>
     <div class="bundles">
         <div class="bundle" v-for="bundle in bundles" :key="bundle.id">
-            <h2></h2>
-            <div class="coach"></div>
-            <div class="duration"></div>
-            <div class="deadline"></div>
+            <h2>{{ bundle.user.first_name }} {{ bundle.user.last_name }}</h2>
+            <div class="coach">
+                <p>Coach: {{ bundle.coach.first_name }} {{ bundle.coach.last_name }}</p>
+            </div>
+            <div class="duration">
+                <p>Duration: {{ bundle.duration_mins }} mins</p>
+            </div>
+            <div class="deadline">
+                <p>Deadline: {{ bundle.deadline }}</p>
+            </div>
             <div class="session"></div>
         </div>
     </div>
