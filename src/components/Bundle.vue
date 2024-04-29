3000e0973c11739096c455b001d9ebc9f4f43ba9
Michael McCann <micanio@googlemail.com>
2024-04-29 10:18:09 +0100

    Added Bundle.vue file

---
+++ src/components/Bundle.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="bundles">
+        <div class="bundle">
+            <div class="coach"></div>
+            <div class="duration"></div>
+            <div class="deadline"></div>
+            <div class="session"></div>
+        </div>
+    </div>
+</template>
